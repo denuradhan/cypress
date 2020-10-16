22bafe6cd2b80b2b1109da124d145db0c33adc38
denuradhan <denny.dand@outlook.com>
2020-10-16 12:56:44 +0700

    adding testing code to sample_spec.js

--- cypress/integration/sample_spec.js
+++ cypress/integration/sample_spec.js
@@ -1,5 +1,26 @@
-describe('My First Test', () => {
-    it('Does not do much!', () => {
-      expect(true).to.equal(true)
-    })
+/// <reference types="cypress" />
+
+describe('Form input', () => {
+    it('visit form', () => {
+        cy.visit('/')
+        cy.contains('Forms').click()
+        cy.contains('Form Layouts').click()
+    });
+
+    it('Fill Inline form', () => {
+        cy.get('input[placeholder="Jane Doe"]').type('Denny Nur Ramadhan')
+        cy.get('[placeholder="Email"][type="text"]').type('denny.dand@outlook.com')
+        cy.get('.custom-checkbox').eq(0).click();
+    });
+
+    it('Fill Using the Grid', () => {
+        cy.get('#inputEmail1').type('denny.dand@outlook.com')
+        cy.get('#inputPassword2').type('Killer_queen666')
+        cy.get('.text').contains('Option 1').click()
+        cy.get('.text').contains('Option 2').click()
+    });
+
+
+
+    
 })
