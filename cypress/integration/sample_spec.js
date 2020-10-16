9fb8822b87e3984ec43615d379cf402e44b256d7
denuradhan <denny.dand@outlook.com>
2020-10-16 12:58:58 +0700

    adding new testing code to sample_spec.js

--- cypress/integration/sample_spec.js
+++ cypress/integration/sample_spec.js
@@ -20,7 +20,30 @@ describe('Form input', () => {
         cy.get('.text').contains('Option 2').click()
     });
 
+    it('Fill Basic form', () => {
+      cy.get('#exampleInputEmail1').type('denny.dand@outlook.com')
+      cy.get('#exampleInputPassword1').type('Killer_queen666')
+      cy.get('.custom-checkbox').eq(1).click()
+    });
 
+    it('Fill Form without labels', () => {
+        cy.get('[placeholder="Recipients"]').type('Denny Nur Ramadhan')
+        cy.get('[placeholder="Subject"]').type('Lorem Ipsum')
+        cy.get('textarea[placeholder="Message"]').type('Lorem ipsum dolor sit amet')
+    });
+
+    it('Fill Block form', () => {
+      cy.get('#inputFirstName').type('Denny')
+      cy.get('#inputLastName').type('Nur')
+      cy.get('#inputEmail').type('denny.dand@outlook.com')
+      cy.get('#inputWebsite').type('www.denny.com')
+    });
+
+    it('Fill Horizontal form', () => {
+        cy.get('#inputEmail3').type('denny.dand@outlook.com')
+        cy.get('#inputPassword3').type('Killer_queen666')
+        cy.get('.custom-checkbox').eq(2).click();
+    });
 
     
 })
